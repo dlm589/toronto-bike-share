3580891d9095151ea57befa51e3cf200a5dde659
dlm589 <46724026+dlm589@users.noreply.github.com>
2024-06-03 23:21:20 -0400

    Update

---
+++ docs/_app/immutable/entry/start.K2u2yynR.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CENXUzbX.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,7 @@
 
     onMount(async () => {
         // Load CSV data
-        csv = await d3.csv(`src/data/${sheet}.csv`);
+        csv = await d3.csv(`data/${sheet}.csv`);
         console.log(csv)
 
         // get the header of the csv
